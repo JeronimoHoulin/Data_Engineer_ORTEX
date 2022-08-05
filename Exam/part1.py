46a1761df50f04d8cf3534b994bfc224896555ee
JeronimoHoulin <jeronimo.houlin@gmail.com>
2022-08-05 17:57:00 +0200

    Fixed first exams PEP8 errs

--- Exam/part1.py
+++ Exam/part1.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Aug  5 11:05:56 2022
 
@@ -10,35 +9,40 @@ import pandas as pd
 
 df = pd.read_csv("2017.csv")
 
-#Editing data types on th Pandas df so that it can merge with postgradeSQL
+"""Editing data types on th Pandas df so that it can merge with postgradeSQL"""
 replacements = {
     'object': 'varchar',
     'int64': 'int'
 }
 df.dtypes.replace(replacements)
 
-df = df [["exchange", "inputdate", "companyName", "valueEUR", "tradeSignificance"]]
+df = df[[
+    "exchange", "inputdate", "companyName", "valueEUR", "tradeSignificance"
+    ]]
 
 df.to_csv('trades_recap.csv', header=df.columns, index=False, encoding='utf-8')
 
 my_file = open('trades_recap.csv')
 
 
-#Openning connection to PostgradeSQL db
+"""Openning connection to PostgradeSQL db"""
 conn = psycopg2.connect(
-   database="2017", user='postgres', password='', host='127.0.0.1', port= '5432'
+    database="2017", 
+    user='postgres', 
+    password='', 
+    host='127.0.0.1', 
+    port='5432'
 )
 cursor = conn.cursor()
 
 cursor.execute("select version()")
 
 data = cursor.fetchone()
-#print("Connection established to: ", data)
 
-#Doping TRADES table if already exists.
+"""Doping TRADES table if already exists."""
 cursor.execute("DROP TABLE IF EXISTS TRADES")
 
-sql ='''CREATE TABLE TRADES(
+sql = '''CREATE TABLE TRADES(
    EXCHANGE VARCHAR NOT NULL,
    INPUTDATE INT,
    COMPANYNAME VARCHAR,
@@ -46,11 +50,9 @@ sql ='''CREATE TABLE TRADES(
    TRADESIG INT
 )'''
 cursor.execute(sql)
-#print("Table created successfully!.")
 conn.commit()
 
-#Pushing the 2017.csv data into the table
-
+"""Pushing the 2017.csv data into the table"""
 sql_upload = """
     COPY TRADES FROM STDIN WITH
     CSV
@@ -58,9 +60,8 @@ sql_upload = """
 """
 
 cursor.copy_expert(sql=sql_upload, file=my_file)
-#print("File pushed to db!")
 
-#First question:
+"""First question:"""
 sql = """
     SELECT EXCHANGE, Count(EXCHANGE) as TotalTrades
     FROM TRADES
@@ -71,28 +72,39 @@ sql = """
 cursor.execute(sql)
 resp = cursor.fetchall()
 print("\n")
+print("Question 1")
 print("TOP 3 Exchanges by trades:")
 print(f" #1: {resp[0][0]} with {resp[0][1]} trades.")
 print(f" #2: {resp[1][0]} with {resp[1][1]} trades.")
 print(f" #3: {resp[2][0]} with {resp[2][1]} trades.")
 
-#Second question
+"""Second question"""
 sql = """
     SELECT * FROM TRADES
     WHERE 20170801 <= INPUTDATE and INPUTDATE < 20170901
 """
 cursor.execute(sql)
 resp = cursor.fetchall()
-df_august = pd.DataFrame(resp, columns =['exchange', 'date', 'name', 'eur', 'significance'])
+df_august = pd.DataFrame(resp, columns=[
+    'exchange', 
+    'date', 
+    'name', 
+    'eur', 
+    'significance'])
 
-sum_company = df_august.groupby(["name"]).sum().sort_values(["eur"], ascending=False).reset_index()
+sum_company = df_august.groupby(
+    ["name"]
+    ).sum().sort_values(
+        ["eur"], ascending=False).reset_index()
 top2 = sum_company[0:2]
-print("\n")
-print("Companies with highest ValueEUR in August:")
-print(f" #1: {top2.name[0]} with ${'{:,.2f}'.format(round(top2.eur[0], 1))} Euros.")
-print(f" #2: {top2.name[1]} with ${'{:,.2f}'.format(round(top2.eur[1], 1))} Euros.")
 
-#Third question
+print("\n")
+print("Question 2")
+print("Companies with highest ValueEUR in August:")
+print(f" #1: {top2.name[0]} with ${'{:,.2f}'.format(round(top2.eur[0], 1))}.")
+print(f" #2: {top2.name[1]} with ${'{:,.2f}'.format(round(top2.eur[1], 1))}.")
+
+"""Third question"""
 sql = """
     SELECT * FROM TRADES
     WHERE TRADESIG = 3
@@ -100,54 +112,32 @@ sql = """
 cursor.execute(sql)
 resp = cursor.fetchall()
 
-df_3 = pd.DataFrame(resp, columns =['exchange', 'date', 'name', 'eur', 'significance'])
-#I will use Value EUR as the referance for transaction volume, as it will be displayed in %.. so it doesn't matter if its USD
-df_3['month'] =  pd.to_datetime(df_3['date'], format='%Y%m%d').dt.month
+df_3 = pd.DataFrame(resp, columns=[
+    'exchange',
+    'date',
+    'name',
+    'eur',
+    'significance'])
+
+"""
+I will use Value EUR as the referance for transaction volume, 
+as it will be displayed in %.. so it doesn't matter if its USD
+"""
+df_3['month'] = pd.to_datetime(df_3['date'], format='%Y%m%d').dt.month
 
 df_3['percent'] = df_3['eur'].transform(lambda x: (x/x.sum()) * 100)
 
 monthly_perc = df_3.groupby(["month"]).sum()
-#print(round(monthly_perc["percent"].sum()))
 
 monthly_perc.drop(["date", "eur", "significance"], axis=1, inplace=True)
 
 monthly_perc["percent"] = round(monthly_perc["percent"], 2)
 
 print("\n")
+print("Question 3")
 print("Monthly percentage of transactions with trade significance 3: ")
 print("\n")
 print(monthly_perc)
-#Closing the connection
+
+"""Closing the connection"""
 conn.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Exam/part2.py
+++ Exam/part2.py
@@ -11,11 +11,42 @@ import numpy as np
 df = pd.read_csv("2017.csv")
 #list(df.columns.values)
 
+#Quesiton 1
+stats = pd.DataFrame()
+for i in df.source.unique():
+    
+    country_df = df[df["source"] == i].transactionType.value_counts()
+    stats.loc[i, 'Buy'] = country_df.Buy
+    stats.loc[i, 'Sell'] = country_df.Sell
+    
+    stats["ratio"] = stats["Buy"]/stats["Sell"]
+    
+    stats.loc[i, "shares"] = df[df["source"] == i].shares.sum() 
+    
+
+    
+stats["weights"] = stats['shares'].transform(lambda x: (x/x.sum()) * 100)
+#print(stats["weights"].sum())
+
+stats["weighted_ratio"] = stats["ratio"] * stats["weights"]
+    
+stats.sort_values(["weighted_ratio"], ascending=False).reset_index()
+
+
+print("\n")
+print("Question 1")
+print("The top 3 sources by weighted Buy & Sell ratio are:")
+print(f" #1 {stats.index[0]}.")
+print(f" #2 {stats.index[1]}.")
+print(f" #3 {stats.index[2]}.")
+
+
 #Quesiton 2
 sumed_value = df.groupby(["currency"]).sum().sort_values(["value"], ascending=False).reset_index()
 top3 = sumed_value[0:3]
 
 print("\n")
+print("Question 2")
 print("Top 3 currencies by value traded:")
 print(f" #1 {top3.currency[0]} with ${'{:,.2f}'.format(round(top3.value[0]))} in traded value.")
 print(f" #2 {top3.currency[1]} with ${'{:,.2f}'.format(round(top3.value[1]))} in traded value.")
@@ -25,4 +56,5 @@ print(f" #3 {top3.currency[2]} with ${'{:,.2f}'.format(round(top3.value[2]))} in
 inrange = df[(df['inputdate'] - df['tradedate'] > 7*2)]
 
 print("\n")
+print("Question 3")
 print(f"The total number of transactions with Input date > 2 weeks from Trade date is {len(inrange)}.")
